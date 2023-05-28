f9ffea75e534a8dafc6a5b20d952d2e023d64d36
DauserP1 <ahmadalsoub3223@gmail.com>
2023-05-28 12:41:59 +0000

    changes

--- app.js
+++ app.js
@@ -47,35 +47,81 @@ employeeSalary.push(netSalary);
 return netSalary;
 }
 
+
+
 employee.prototype.display = function(allEmployees) {
+    
+    
+    
     for (var i = 0; i < allEmployees.length; i++) {
-      document.write("<br>");
-      document.write(".   Name : "+allEmployees[i].fullName+"<br>");
-      document.write(".   Salary : "+employeeSalary[i]+"<br>");
+
+        let div = document.createElement("div");
+        div.style.border="solid 2px";
+        div.style.height="415px";
+        div.style.width="250px";
+        let body = document.getElementsByTagName("body")[0];
+        body.appendChild(div);
+        
+        let image = document.createElement("img");
+        image.src = allEmployees[i].ImageURL;
+        image.style.height="250px";
+        image.style.width="250px";
+        image.style.padding="30px";
+        div.appendChild(image);
+    
+        let name = document.createElement("h3");
+        name.textContent="Name : "+allEmployees[i].fullName;
+        name.style.padding="10px";
+        name.style.fontFamily = "monospace";
+        div.appendChild(name);
+    
+        let id = document.createElement("h3");
+        id.textContent= "ID : "+allEmployees[i].uniqeID(existingID);
+        id.style.padding="10px";
+        id.style.fontFamily = "monospace";
+        div.appendChild(id);
+    
+        let departmentName = document.createElement("h3");
+        departmentName.textContent="Name : "+allEmployees[i].department;
+        departmentName.style.padding="10px";
+        departmentName.style.fontFamily = "monospace";
+        div.appendChild(departmentName);
+
+        let salary = document.createElement("h3");
+        salary.textContent= "salary : "+allEmployees[i].salary(this.level);
+        salary.style.padding="10px";
+        salary.style.fontFamily = "monospace";
+        div.appendChild(salary);
+        
+
+        document.body.appendChild(div);
+     
+     
+     
       
     }
 }
 //creating objects
 
-var Ghazi = new employee('Ghazi Samer','Administration','Senior' ,'');
+var Ghazi = new employee('Ghazi Samer','Administration','Senior' ,'./assets/Ghazi.jpg');
 Ghazi.uniqeID(existingID);
 Ghazi.salary(Ghazi.level);
-var Lana = new employee('Lana Ali','Finance','Senior','');
+var Lana = new employee('Lana Ali','Finance','Senior','./assets/Lana.jpg');
 Lana.uniqeID(existingID);
 Lana.salary(Lana.level);
-var Tamara = new employee('Tamara Ayoub','Marketing','Senior','');
+var Tamara = new employee('Tamara Ayoub','Marketing','Senior','./assets/Tamara.jpg');
 Tamara.uniqeID(existingID);
 Tamara.salary(Tamara.level);
-var Safi = new employee('Safi Walid','Administration','Mid-Senior','');
+var Safi = new employee('Safi Walid','Administration','Mid-Senior','./assets/Safi.jpg');
 Safi.uniqeID(existingID);
 Safi.salary(Safi.level);
-var Omar = new employee('Omar Zaid','Development','Senior','');
+var Omar = new employee('Omar Zaid','Development','Senior','./assets/Omar.jpg');
 Omar.uniqeID(existingID);
 Omar.salary(Omar.level);
-var Rana = new employee('Rana Saleh','Development','Junior' ,'');
+var Rana = new employee('Rana Saleh','Development','Junior' ,'./assets/Rana.jpg');
 Rana.uniqeID(existingID);
 Rana.salary(Rana.level);
-var Hadi = new employee('Hadi Ahmad','Finance','Mid-Senior' ,'')
+var Hadi = new employee('Hadi Ahmad','Finance','Mid-Senior' ,'./assets/Hadi.jpg')
 Hadi.uniqeID(existingID);
 Hadi.salary(Hadi.level);
 
